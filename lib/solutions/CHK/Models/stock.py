9b75622f7078dc9b64db8cde812c14159a55e9b0
sbx_user1051 <sbx_user1051@169.254.36.217>
2022-03-27 23:33:32 +0000

    Sun Mar 27 23:33:32 UTC 2022

--- lib/solutions/CHK/Models/stock.py
+++ lib/solutions/CHK/Models/stock.py
@@ -1,15 +1,14 @@
 from dataclasses import dataclass
-
-
 from lib.solutions.CHK.Models.product import Product
 
 
 @dataclass
 class Stock:
-    stock: set(Product)
+    stock: set() = None
 
-    def check_item_in_stock(self, product: Product):
-        return product in self.stock
+    # def check_item_in_stock(self, product: Product):
+    #     return product in self.stock
+
 
 
 
