616919666ce9a3725c19d82271727cf927dd6a1d
sbx_user1051 <sbx_user1051@169.254.65.253>
2022-03-27 22:39:20 +0000

    Sun Mar 27 22:39:20 UTC 2022

--- lib/solutions/CHK/product.py
+++ lib/solutions/CHK/product.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
-
+from typing import Optional
 from lib.solutions.CHK.offer import Offer
 
 
 @dataclass
-class Product(Offer):
+class Product:
     sku: str
     price: int = None
-    offer: dict = None
+    offer: Optional[set(Offer)] = None
 
     def __hash__(self):
         return hash(self.sku)
 
     def __eq__(self, product):
         return self.sku == product.sku
+
