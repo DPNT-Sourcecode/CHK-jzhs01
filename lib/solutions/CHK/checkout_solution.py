4436ce0db7ec60c7665fc9be04825b96e46aa9da
sbx_user1051 <sbx_user1051@169.254.101.29>
2022-03-27 22:00:04 +0000

    Sun Mar 27 22:00:04 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,25 +10,16 @@ from lib.solutions.CHK.product import Product
        item in the basket TC = O(n) and SC = O(n)
      - For every key (product name) we can check for the price and whether there is a promo.
 
-     This is where I think of two approaches (without setting up a DB):
-
-     1. We could build a separate class for "stock" where init would have two dictionaries.
-        Dict 1 = prices<sku, price>
-        Dict 2 = deals<sku,deal<count, price>>
-     Pros:
-     - Quick access time: O(1) to access both the the deals and the prices for every SKU
-     Cons:
-     - Not very OOP
-
-     2. Build a class of "Product" consisting of Product(sku:str, price:int, deal:dict<quantity, price>) 
-        Build an additional class "Stock" that would store all of the stock as a list of "Product" classes
-     Pros:
+     Build a class of "Product" consisting of Product(sku:str, price:int) 
+     Build an additional class "Stock" that would store all of the stock as a list of "Product" classes
+     
      - Very OOP
+     - Items can be easily added to the stock
      - Item can be later reused as a model when DB will be used
-     Cons:
-     - Runtime for the current question, O(n) to find the product in stock
-
-     I will implement the first approach now for the runtime consideration
+     - Runtime for the current question, O(1) to find the product in stock 
+       if we make stock as a set
+    
+    
     """
 
 
