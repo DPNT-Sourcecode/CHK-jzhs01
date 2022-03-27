521af4fda0fbb8b95f0c1987da28fd8e020fa70c
sbx_user1051 <sbx_user1051@169.254.122.121>
2022-03-27 16:31:42 +0000

    Sun Mar 27 16:31:42 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -2,10 +2,12 @@ from lib.solutions.SUM import sum_solution
 
 
 class TestSum():
-    def no_arguments_passed_in(self):
-        assert  sum_solution.compute() == 0
+
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_no_arguments_passed_in(self):
+        assert sum_solution.compute() == 0
 
     def test_edge_case_zeros(self):
         assert sum_solution.compute(0, 0) == 0
@@ -14,5 +16,6 @@ class TestSum():
         assert sum_solution.compute(100, 100) == 200
 
     def test_string_argument(self):
-        assert sum_solution.compute(10,)
+        assert sum_solution.compute(10, )
+
 
