d760726f029f66becaa680184e522efed7377be2
sbx_user1051 <sbx_user1051@169.254.2.29>
2022-03-27 18:12:55 +0000

    Sun Mar 27 18:12:55 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,5 +15,32 @@ def checkout(skus):
         total value of all items in the basket.
         It will be an int and not a float since the price of every item is a whole number
     """
+    """
+    NOTE: I will denote time complexity as TC, and space complexity as SC 
+    Initial thoughts:
+     - Given a string, we can use collections.Counter to build a hash map of count of every
+       item in the basket TC = O(n) and SC = O(n)
+     - For every key (product name) we can check for the price and whether there is a promo.
+     
+     This is where I think of two approaches (without setting up a DB):
+     
+     1. We could build a separate class for "stock" where init would have two dictionaries.
+        Dict 1 = prices<sku, price>
+        Dict 2 = deals<sku,deal<count, price>>
+     Pros:
+     - Quick access time: O(1) to access both the the deals and the prices for every SKU
+     Cons:
+     - Not very OOP
+     
+     2. Build a class of "Product" consisting of Product(sku:str, price:int, deal:dict<quantity, price>) 
+        Build an additional class "Stock" that would store all of the stock as a list of "Product" classes
+     Pros:
+     - Very OOP
+     - Item can be later reused as a model when DB will be used
+     Cons:
+     - Runtime for the current question, O(n) to find the product in stock
+     
+     I will implement the first approach now for the runtime consideration
+    """
 
 
---
+++ lib/solutions/CHK/stock.py
@@ -0,0 +1,16 @@
+from lib.solutions.CHK.product import Product
+
+
+class Stock(Product):
+
+    def __init__(self):
+        self.stock = [
+            Product(sku="A", price=50, offer={3: 130}),
+            Product(sku="B", price=30, offer={2: 45}),
+            Product(sku="C", price=20),
+            Product(sku="D", price=15)
+                 ]
+
+    def view_stock(self):
+        return self.stock
+
