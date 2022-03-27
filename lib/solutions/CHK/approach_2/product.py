96c4cc0e0ed0dee8642b4e0e4b6eaa37e0d4d5f0
sbx_user1051 <sbx_user1051@169.254.15.237>
2022-03-27 20:21:23 +0000

    Sun Mar 27 20:21:23 UTC 2022

--- lib/solutions/CHK/approach_2/product.py
+++ lib/solutions/CHK/approach_2/product.py
@@ -1,5 +1,6 @@
 class Product:
-    def __init__(self, sku: str, price: int, offer: dict = None):
+    def __init__(self, sku: str, price: int = None, offer: dict = None):
         self.sku = sku
         self.price = price
         self.offer = offer
+
