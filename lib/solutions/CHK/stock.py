1ec6d74a56398f8bf2fe6b32e911dbad5ec2fd88
sbx_user1051 <sbx_user1051@169.254.8.173>
2022-03-27 22:29:41 +0000

    Sun Mar 27 22:29:41 UTC 2022

--- lib/solutions/CHK/stock.py
+++ lib/solutions/CHK/stock.py
@@ -2,11 +2,19 @@ from dataclasses import dataclass
 from lib.solutions.CHK.product import Product
 
 
-@dataclass
-class Stock(Product):
-    current_stock: dict
+class Stock:
+
+    def __init__(self):
+        self.stock = {
+            Product(sku="A", price=50, offer={3: 130}),
+            Product(sku="B", price=30, offer={2: 45}),
+            Product(sku="C", price=20),
+            Product(sku="D", price=15)
+        }
 
     def check_item_in_stock(self, product: Product):
         return product in self.stock
+
+
 
 
