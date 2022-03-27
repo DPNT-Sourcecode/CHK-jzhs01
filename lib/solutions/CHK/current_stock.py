9b0da0323a76e669a4aa55c264b9b0aefd70be45
sbx_user1051 <sbx_user1051@169.254.36.217>
2022-03-27 23:51:32 +0000

    Sun Mar 27 23:51:32 UTC 2022

--- lib/solutions/CHK/current_stock.py
+++ lib/solutions/CHK/current_stock.py
@@ -2,19 +2,21 @@ from lib.solutions.CHK.Models.offer import PriceReduction  # , FreeItem
 from lib.solutions.CHK.Models.product import Product
 from lib.solutions.CHK.supermarket_stock import Stock
 
-# productA = Product(sku="A", price=50,
-#                    offer={PriceReduction(product_count=5, new_price=200),
-#                           PriceReduction(product_count=3, new_price=130)})
+# Offers
+productA_offer_1 = PriceReduction(product_count=5, new_price=200)
+productA_offer_2 = PriceReduction(product_count=3, new_price=130)
+productB_offer_1 = PriceReduction(product_count=2, new_price=45)
+# Products
+productA = Product(sku="A", price=50, offer={productA_offer_1, productA_offer_2})
 productB = Product(sku="B", price=30,
-                   offer={PriceReduction(product_count=2, new_price=45)})
+                   offer={productB_offer_1})
 productC = Product(sku="C", price=20)
 productD = Product(sku="D", price=15)
 productE = Product(sku="E", price=40,
                    # offer={FreeItem(product_count=2, product=productB, count=1)}
                    )
 
-current_stock = Stock(stock=(
-    # productA,
-    productB, productC, productD, productE))
+current_stock = Stock(stock=(productA, productB, productC, productD, productE))
+
 
 
