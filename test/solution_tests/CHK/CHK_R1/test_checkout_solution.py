2630219f19682857f06d27ba06d0917a7d37ce7c
sbx_user1051 <sbx_user1051@169.254.228.125>
2022-03-27 18:40:06 +0000

    Sun Mar 27 18:40:06 UTC 2022

--- test/solution_tests/CHK/CHK_R1/test_checkout_solution.py
+++ test/solution_tests/CHK/CHK_R1/test_checkout_solution.py
@@ -1,5 +1,18 @@
 from lib.solutions.CHK import checkout_solution
 
 
-def test_checkout():
+def test_checkout_empty():
+    assert checkout_solution.checkout("") == 0
+
+
+def test_checkout_AAA():
     assert checkout_solution.checkout("AAA") == 130
+
+
+def test_checkout_BB():
+    assert checkout_solution.checkout("BB") == 45
+
+
+def test_checkout_CC():
+    assert checkout_solution.checkout("CC") == 40
+
