2587bc82ca9177f488ee14f1a89dc7ab25af5bde
sbx_user1051 <sbx_user1051@169.254.2.29>
2022-03-27 17:33:55 +0000

    Sun Mar 27 17:33:55 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,13 +4,16 @@
 # skus = unicode string
 def checkout(skus):
     """
-    Arguments
+    Args:
     --------------
     skus: str
         A string of all skus in the basket
 
-         
-
+    Returns
+    --------------
+    @return: int
+        total value of all items
     """
     raise NotImplementedError()
+
 
