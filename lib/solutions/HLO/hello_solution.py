80e45dd93e491e65bda9c5f5ef91586e8c09ad86
sbx_user1051 <sbx_user1051@169.254.17.29>
2022-03-27 17:11:38 +0000

    Sun Mar 27 17:11:38 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,6 +4,11 @@
 # friend_name = unicode string
 def hello(friend_name):
     """
-    
+    @args
+    friend_name: str, a name of a friend
+
+    @return
+
     """
+    return f"{str(friend_name)}, hello!"
 
