2b29697bb1d259183a79f3e44ce2f408b73b2ee5
sbx_user1051 <sbx_user1051@169.254.2.29>
2022-03-27 17:45:55 +0000

    Sun Mar 27 17:45:55 UTC 2022

---
+++ lib/solutions/CHK/product.py
@@ -0,0 +1,5 @@
+class Product:
+    def __init__(self, sku:, price, offer):
+        self.sku = sku
+        self.price = price
+        self.offer = offer
---
+++ lib/solutions/CHK/supermarket_stock.py
@@ -0,0 +1,6 @@
+class Stock:
+
+    def __init__(self):
+        self.products = {"A": 50, "B": 30, "C": 20, "D": 15}
+        self.offers = {"A": [3, 150], "B": [2, 45]}
+
