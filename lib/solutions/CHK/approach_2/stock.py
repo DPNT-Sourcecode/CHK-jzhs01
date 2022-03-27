e19c398cb85c679e73260b8e9889581884194e6d
sbx_user1051 <sbx_user1051@169.254.15.237>
2022-03-27 20:24:23 +0000

    Sun Mar 27 20:24:23 UTC 2022

--- lib/solutions/CHK/approach_2/stock.py
+++ lib/solutions/CHK/approach_2/stock.py
@@ -1,12 +1,13 @@
 from lib.solutions.CHK.approach_2.product import Product
 
 
-class Stock(Product):
+class Stock():
 
     def __init__(self):
         self.stock = {Product(sku="A", price=50, offer={3: 130}),Product(sku="B", price=30, offer={2: 45}),Product(sku="C", price=20),Product(sku="D", price=15)}
 
     def __eq__(self, product):
         return self.sku == product.sku
+
 
 
