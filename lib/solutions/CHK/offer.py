b394964c9f4aba1835fbebb8668db6dea1f9ad85
sbx_user1051 <sbx_user1051@169.254.36.217>
2022-03-27 23:03:32 +0000

    Sun Mar 27 23:03:32 UTC 2022

--- lib/solutions/CHK/offer.py
+++ lib/solutions/CHK/offer.py
@@ -1,9 +1,19 @@
 from dataclasses import dataclass
 
+from lib.solutions.CHK.product import Product
+
 
 @dataclass
 class Offer:
     product_count: int
 
-class T
+
+class PriceReduction(Offer):
+    new_price: int
+
+
+class FreeItem(Offer):
+    item: Product
+    count: int = 1
+
 
