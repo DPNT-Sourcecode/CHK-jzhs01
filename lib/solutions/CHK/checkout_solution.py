f0f1765e04a82aa818f78ebb26e35e31c05dad8e
sbx_user1051 <sbx_user1051@169.254.2.29>
2022-03-27 17:30:55 +0000

    Sun Mar 27 17:30:55 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    """
+    Arguments
+    --------------
+    skus: str
+        A string of all skus in the basket
+
+         
+
+    """
     raise NotImplementedError()
+
