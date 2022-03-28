49c4391740c2b6c5d2b06137895d5350bd541a5d
sbx_user1051 <sbx_user1051@169.254.36.217>
2022-03-28 01:03:32 +0000

    Mon Mar 28 01:03:32 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,4 @@
 from lib.solutions.CHK.Models.offer import PriceReduction, FreeItem
-from lib.solutions.CHK.Models.product import Product
 from lib.solutions.CHK.current_stock import current_stock
 
 # noinspection PyUnusedLocal
@@ -34,47 +33,28 @@ def checkout(skus: str) -> int:
         else:
             return -1
 
-    for sku, quantity in basket.items():  # TC, SC = O(n), O(n)
-        # Offer Available
-        product = current_stock[sku]
-        if product.offer:
-            for offer in product.offer:
-
-                # Case when price reduction offer
-                if offer is isinstance(offer, PriceReduction()):
-                    product_count, new_price = offer.product_count, offer.new_price
-                    total += quantity // product_count * new_price
-                    quantity %= product_count
-
-                # Case when free item offer
-                elif offer is isinstance(offer, FreeItem()):
-                    product_count, product, count = offer.product_count, offer.product, count
-                    skus.append([product.sku] * quantity // product_count * count)
-        else:
-            total += quantity * product.price
-
-    return total
-
-    # for sku, quantity in basket.items():  # <SC, TC> = O(n), O(n)
+    return True
+    # for sku, quantity in basket.items():  # TC, SC = O(n), O(n)
+    #     # Offer Available
+    #     product = current_stock[sku]
+    #     if product.offer:
+    #         for offer in product.offer:
     #
-    #     # Accounting for the characters that are not stock
-    #     if sku not in prices:
-    #         return -1
+    #             # Case when price reduction offer
+    #             if offer is isinstance(offer, PriceReduction()):
+    #                 product_count, new_price = offer.product_count, offer.new_price
+    #                 total += quantity // product_count * new_price
+    #                 quantity %= product_count
     #
-    #     # Get total value for the deal
-    #     if sku in deals:
-    #         deal_quantity, price = deals[sku]
-    #         total += quantity // deal_quantity * price
-    #         quantity %= deal_quantity
-    #
-    #     # Get value for the items that we have a record for SKU
-    #     if sku in prices:
-    #         total += quantity * prices[sku]
+    #             # Case when free item offer
+    #             elif offer is isinstance(offer, FreeItem()):
+    #                 product_count, free_product, count = offer.product_count, offer.product, count
+    #                 skus.append([free_product.sku] * quantity // product_count * count)
+    #     else:
+    #         total += quantity * product.price
     #
     # return total
-    #
-    # from lib.solutions.CHK.product import Product
-    # return Product(sku="Z") in Stock().stock
+
 
 
 
